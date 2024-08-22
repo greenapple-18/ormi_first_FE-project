f5aaa10ddedef34ffc3420c21ab9c85956d3b848
greenapple <dla16253745@gmail.com>
2024-08-22 23:46:27 +0900

    [Update] Update menu header

--- default_JS_setting.js
+++ default_JS_setting.js
@@ -2,18 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const topBtn = document.getElementById("top_btn");
     const footer = document.querySelector("footer");
     const header = document.querySelector("header");
-
+    const headerAll = document.querySelectorAll("header");
     handleScroll();
 
     function handleScroll() {
         const footerArea = footer.getBoundingClientRect();
+        const headerArea = header.getBoundingClientRect();
+        const headerSecondArea = headerAll[1].getBoundingClientRect();
         const scroll = window.scrollY;
 
-        if (scroll > 0) {
+        if (headerArea.height > headerSecondArea.bottom) {
             header.style.position = "fixed";
+            header.style.transform = "translateY(0)";
+            header.style.transition = "ransform 0.3s ease";
         } else {
-            header.style.position = "static";
+            header.style.position = "absolute";
         }
+
+        // else if (scroll < headerArea.bottom) {
+        //     header.style.top = `${scroll - headerArea.height}px`;
+        //     header.style.transform = "translateY(0)";
+        // }
 
         if (scroll > 300) {
             topBtn.style.display = "block";
