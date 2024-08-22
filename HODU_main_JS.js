0a4c1dc6650425d14c35526281b8ea913a1ae27b
greenapple <dla16253745@gmail.com>
2024-08-22 23:45:26 +0900

    [Update] Add modal function Update : HODU 랜딩페이지.html, HODU_main_CSS.css Add : HODU_main_JS.js, Modal_background.png, Modal_section_cat.png

---
+++ HODU_main_JS.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const subscribeButton = document.getElementById("subscribeButton");
+    const emailInput = document.getElementById("emailInput");
+    const modal = document.getElementById("modal");
+    const modalOkButton = document.getElementById("modalOkButton");
+
+    modalOkButton.addEventListener("click", () => {
+        closeModal();
+    });
+
+    subscribeButton.addEventListener("click", function () {
+        const email = emailInput.value;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!email) {
+            alert("이메일을 입력해주세요.");
+            return;
+        } else if (!emailPattern.test(email)) {
+            alert("올바른 이메일 형식으로 입력해주세요.\n(ex. abcd@abcd.abc)");
+            return;
+        } else {
+            modal.style.display = "flex";
+            document.body.classList.add("modal-open");
+            emailInput.value = "";
+        }
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    window.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            closeModal();
+        }
+    });
+
+    function closeModal() {
+        modal.style.display = "none";
+        document.body.classList.remove("modal-open"); // 스크롤 해제
+    }
+});
